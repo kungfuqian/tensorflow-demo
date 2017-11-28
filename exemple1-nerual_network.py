007166940d6fb80b9d8da70d3625c55ca01133f8
qian <freedanfan@gmail.com>
2017-11-28 05:48:25 -0800

    change 1

---
+++ exemple1-nerual_network.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import numpy as np
+
+#create data
+
+x_data = np.random.rand(100).astype(np.float32)
+y_data = 0.1*x_data + 0.3
+
+### create tensorflow structure start  
+
+Weights = tf.Variable(tf.random_uniform([1],-1.0,1.0))
+diases = tf.Variable(tf.zeros([1]))
+
+y = Weights*x_data + diases
+loss = tf.reduce_mean(tf.square(y-y_data))
+
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+train = optimizer.minimize(loss)
+
+init = tf.global_variables_initializer()
+
+### create tensorflow structure end ###
+
+
+sess = tf.Session()
+sess.run(init)
+
+for step in range(201):
+	sess.run(train)
+	if step % 20 == 0:
+		print(step, sess.run(Weights),sess.run(diases))
