d28ee53ee55e0c46df97543892d2999dd220c0c5
qian <freedanfan@gmail.com>
2017-11-28 21:56:36 -0800

    get the better result in 2.

--- 2.git_and_tensorflow_test.py
+++ 2.git_and_tensorflow_test.py
@@ -10,13 +10,13 @@ y_data = 0.3*x_data + 0.1
 
 # tensorflow structure
 
-Weights = tf.Variable(tf.random_uniform([1],-1,1))
-biases = tf.Variable(tf.zeros([1]))
+Weights = tf.Variable(tf.random_uniform([1],0.29,0.31))
+biases = tf.Variable(0.999*tf.ones([1]))
 
 y = Weights*x_data + biases
 loss = tf.reduce_mean(tf.square(y-y_data))
 
-optimizer = tf.train.GradientDescentOptimizer(0.5)
+optimizer = tf.train.GradientDescentOptimizer(0.71)
 train= optimizer.minimize(loss)
 
 init = tf.global_variables_initializer()
@@ -28,4 +28,10 @@ for step in range(201):
 	sess.run(train)
 	if step % 20 == 0:
 		print(step, sess.run(Weights),sess.run(biases))
+
+x = 100
+y = sess.run(Weights)* x + sess.run(biases)
+y2 = Weights*x + biases
+
+print("y=",y,",y2=",y2)
 
