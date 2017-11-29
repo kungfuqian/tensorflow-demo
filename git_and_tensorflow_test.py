4908a10d043864ac6cc87783609d05efb28a7956
qian <freedanfan@gmail.com>
2017-11-28 21:29:07 -0800

    add git

---
+++ git_and_tensorflow_test.py
@@ -0,0 +1,31 @@
+###  the first time uses git and tensorflow 
+# this is an example 
+
+import tensorflow as tf
+import numpy as np
+
+# data prepare
+x_data = np.random.rand(100).astype(np.float32)
+y_data = 0.3*x_data + 0.1
+
+# tensorflow structure
+
+Weights = tf.Variable(tf.random_uniform([1],-1,1))
+biases = tf.Variable(tf.zeros([1]))
+
+y = Weights*x_data + biases
+loss = tf.reduce_mean(tf.square(y-y_data))
+
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+train= optimizer.minimize(loss)
+
+init = tf.global_variables_initializer()
+
+sess = tf.Session()
+sess.run(init)
+
+for step in range(201):
+	sess.run(train)
+	if step % 20 == 0:
+		print(step, sess.run(Weights),sess.run(biases))
+
