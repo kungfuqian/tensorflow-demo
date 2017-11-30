e4e83f0e2baa893fb9aa86b9fd798a1d19b53add
qian <freedanfan@gmail.com>
2017-11-29 18:27:45 -0800

    2 dims

--- 2.git_and_tensorflow_test.py
+++ 2.git_and_tensorflow_test.py
@@ -1,37 +1,51 @@
-###  the first time uses git and tensorflow 
-# this is an example 
-
 import tensorflow as tf
 import numpy as np
 
-# data prepare
-x_data = np.random.rand(100).astype(np.float32)
-y_data = 0.3*x_data + 0.1
+# 1.准备数据
+x_data = np.float32(np.random.rand(2,100))  # 生成 2行 100列的矩阵
+y_data = np.matmul([0.1,0.6],x_data)+ 0.5
 
-# tensorflow structure
 
-Weights = tf.Variable(tf.random_uniform([1],0.29,0.31))
-biases = tf.Variable(0.999*tf.ones([1]))
+#print(x_data,y_data)
 
-y = Weights*x_data + biases
+# 2.构造线性模型
+diases = tf.Variable(tf.zeros([1]))
+Weights = tf.Variable(tf.random_uniform([1,2]))  # 生成1*2 的随机矩阵
+#print(Weights,diases)
+y = tf.matmul(Weights,x_data) + diases
+
+# 3. 求解模型
+# 设置损失函数
 loss = tf.reduce_mean(tf.square(y-y_data))
 
-optimizer = tf.train.GradientDescentOptimizer(0.71)
-train= optimizer.minimize(loss)
+# 构造目标函数 ，选择梯度下降法
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+# 迭代目标： 最小化损失函数
+train = optimizer.minimize(loss)
 
-init = tf.global_variables_initializer()
 
-sess = tf.Session()
-sess.run(init)
+#################################################################
+# 以下是用 tf来解决上面的任务
 
-for step in range(201):
-	sess.run(train)
-	if step % 20 == 0:
-		print(step, sess.run(Weights),sess.run(biases))
+# 初始化所有变量
+init=tf.global_variables_initializer()
 
-x = 100
-y = sess.run(Weights)* x + sess.run(biases)
-y2 = Weights*x + biases
 
-print("y=",y,",y2=",y2)
+## method 1
+# sess = tf.Session()
+# sess.run(init)
+#
+## 开始循环执行
+# for step in range(200):
+# 	sess.run(train)
+# 	if step % 20 == 0:
+# 		print(step,sess.run(Weights),sess.run(diases))
+# sess.close()
 
+# method 2
+with tf.Session() as sess:
+	sess.run(init)
+	for step in range(301):
+		sess.run(train)
+		if step % 100 == 0:
+			print(step, sess.run(Weights),sess.run(diases))
