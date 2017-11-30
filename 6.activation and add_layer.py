e8bd8a42ab0740f0b42c82878e741d841c0f517b
qian <freedanfan@gmail.com>
2017-11-30 05:52:16 -0800

    activation and add_layer

---
+++ 6.activation and add_layer.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+import numpy as np
+
+
+def add_layer(inputs, in_size, out_size, activation_function=None):
+    Weights = tf.Variable(tf.random_normal([in_size,out_size]))
+    biases = tf.Variable(tf.zeros([1,out_size]) + 0.1)
+    Wx_plus_b = tf.matmul(inputs,Weights) + biases
+    if activation_function == None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_function(Wx_plus_b)
+    return outputs
+
+# prepare data
+x_data = np.linspace(-1,1,300)[:, np.newaxis]
+
+#在指定的间隔内返回均匀间隔的数字。numpy.linspace(start, stop, num=300)
+#  np.newaxis的功能是插入新维度
+
+noise = np.random.normal(0, 0.05, x_data.shape)  # add noise
+y_data = np.square(x_data) - 0.5 + noise
+
+
+xs = tf.placeholder(tf.float32,[None,1]) # 随便输入多少个样本，属性为1
+ys = tf.placeholder(tf.float32,[None,1])
+# build first layer
+# 定义隐藏层
+l1 = add_layer(xs,1,10,activation_function=tf.nn.relu) # 输入1个参数，隐藏层有10个神经元，激励函数用 relu
+# 输出层
+prediction = add_layer(l1, 10, 1,activation_function=None)
+
+
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+init= tf.global_variables_initializer()
+
+sess= tf.Session()
+sess.run(init)
+
+for step in range(1000):
+    sess.run(train_step, feed_dict={xs:x_data,ys:y_data})
+    if step % 50 == 0:
+        print(sess.run(loss, feed_dict={xs:x_data,ys:y_data}))
